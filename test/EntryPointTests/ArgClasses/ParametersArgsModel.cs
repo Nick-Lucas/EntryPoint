7b0bcbca1fab17b217dacc0b6bbe111fbd98418f
Nick Lucas <nicholas.lucas06@gmail.com>
2016-12-23 19:05:01 +0000

    Add drill-down tests just for OptionParameter types

---
+++ test/EntryPointTests/ArgClasses/ParametersArgsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using EntryPoint;
+
+namespace EntryPointTests.ArgClasses {
+    public class ParametersArgsModel : BaseArgumentsModel {
+        [OptionParameter(
+            DoubleDashName = "param-1",
+            SingleDashChar = 'a')]
+        public int Param1 { get; set; }
+
+        [OptionParameter(
+            DoubleDashName = "param-2",
+            SingleDashChar = 'b',
+            NullValueBehaviour = ParameterDefaultEnum.CustomValue,
+            CustomDefaultValue = 7)]
+        public int Param2 { get; set; }
+    }
+}
