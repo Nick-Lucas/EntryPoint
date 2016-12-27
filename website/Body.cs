a42ca4eb2a65f50ed1dbe55658b57207ae376a33
Nick Lucas <nicholas.lucas06@gmail.com>
2016-12-27 19:07:43 +0000

    Clean up

--- website/Body.cs
+++ website/Body.cs
@@ -8,12 +8,14 @@ namespace Website {
     class Body {
         /// ## About EntryPoint
         /// 
-        /// An argument parser with an API intentionally designed to be composable and maintainable.
+        /// An argument parser with an designed to be composable, practical and maintainable.
+        /// 
+        /// Parses arguments in the form `UtilityName [-o | --options] [operands]`
         ///
         /// Supports:
         /// 
         /// * .Net Standard 1.6+ (All future.Net releases are built on this)
-        /// * .Net Framework 4.5.1+
+        /// * .Net Framework 4.5+
         ///
         /// Follows the [IEEE Standard](http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap12.html) 
         /// closely, but does include common adblibs such as fully named --option style options.
@@ -26,8 +28,8 @@ namespace Website {
     }
 
     /// ## Basic Usage
-    /// EntryPoint is designed so you can write declarative `ApplicationOptions` 
-    /// classes and then simply pass to EntryPoint for population
+    /// EntryPoint evolves around declarative `ApplicationOptions` 
+    /// classes which are simply passed to EntryPoint for population
     /// 
     /// Let's say we want a utility used like: `UtilityName -s --name Bob 6.1`
     /// 
@@ -37,13 +39,13 @@ namespace Website {
         public SimpleApplicationOptions() : base("SimpleApp") { }
 
         // Option
-        [Option(
-            LongName = "switch", ShortName = 's')]
+        [Option(LongName = "switch", 
+                ShortName = 's')]
         public bool Switch { get; set; }
 
         // Option-Parameter
-        [OptionParameter(
-            LongName = "name", ShortName = 'n')]
+        [OptionParameter(LongName = "name", 
+                         ShortName = 'n')]
         public string Name { get; set; }
 
         // Operand
@@ -54,7 +56,7 @@ namespace Website {
     class SimpleProgram {
         void main(string[] args) {
             
-            // One line parsing of any BaseApplicationOptions implementation
+            // One line parsing of any `BaseApplicationOptions` implementation
             var options = EntryPointApi.Parse<SimpleApplicationOptions>(args);
 
             // Object oriented access to your arguments
@@ -75,6 +77,7 @@ namespace Website {
     /// * **Detail:** Defines an On/Off option for use on the CLI
     /// * **Argument, LongName:** the case in-sensitive name to be used like `--name`
     /// * **Argument, ShortName:** the case sensitive character to be used like `-n`
+    /// * At least one name needs to be provided
     /// 
     /// #### `[OptionParameter(LongName = string, ShortName = char)]`
     /// * **Apply to:** Class Properties
@@ -82,13 +85,13 @@ namespace Website {
     /// * **Detail:** Defines a parameter which can be invoked to provide a value
     /// * **Argument, LongName:** the case in-sensitive name to be used like `--name`
     /// * **Argument, ShortName:** the case sensitive character to be used like `-n`
+    /// * At least one name needs to be provided
     /// 
     /// #### `[Operand(position = int)]`
     /// * **Apply to:** Class Properties
     /// * **Output Types:** Primitive Types, Enums
     /// * **Detail:** Maps a positional operand from the end of a CLI command
-    /// * **Argument, LongName:** the case in-sensitive name to be used like `--name`
-    /// * **Argument, ShortName:** the case sensitive character to be used like `-n`
+    /// * **Argument, Position:** the 1 based position of the Operand
     /// 
     /// #### `[Required]`
     /// * **Apply to:** Class Properties with any Option or Operand Attribute applied
@@ -96,7 +99,7 @@ namespace Website {
     /// 
     /// #### `[Help(detail = string)]`
     /// * **Apply to:** Class Properties with any Option or Operand Attribute applied, or an ApplicationOptions Class
-    /// * **Detail:** Provides custom documentation on an Option, Operand or ApplicationOptions Class, which is consumed by the --help generator
+    /// * **Detail:** Provides custom documentation on an Option, Operand or ApplicationOptions Class, which will be consumed by the --help generator
     ///
 
 
@@ -115,15 +118,15 @@ namespace Website {
         [Help("When this is set, verbose logging will be activated")]
         public bool Verbose { get; set; }
 
-        // A message which *must* be provided, 
+        // A message which *must* be provided by the user 
         [Required]
         [OptionParameter(LongName = "subject",
                          ShortName = 's')]
         [Help("Mandatory Subject to provide")]
         public string Subject { get; set; }
 
-        // An importance level for the message
-        // If not provided is defaulted to `Normal`
+        // An importance level for the message.
+        // If not provided this is defaulted to `Normal`
         // User can provide the value as a number or string (ie. '2' or 'high')
         [OptionParameter(LongName = "importance", 
                          ShortName = 'i')]
@@ -155,7 +158,7 @@ namespace Website {
     /// ## Help Generator
     /// 
     /// EntryPoint provides an automatic Help generator, which always owns the `-h` and `--help` Options.
-    /// If the Help option is provided then EntryPoint will generate Help information on the CLI, and end the program.
+    /// If the Help option is invoked then EntryPoint will generate Help information on the CLI, and end the program.
     /// 
     /// It consumes the following information:
 #if CODE
@@ -172,8 +175,6 @@ namespace Website {
     }
 #endif
 
-    
-    /// 
     class HelpProgram {
         public void main() {
 
@@ -185,7 +186,17 @@ namespace Website {
             /// It can also be generated manually
 #if CODE
             string help = EntryPointApi.GenerateHelp<HelpApplicationOptions>();
+            Console.WriteLine(help);
 #endif
         }
     }
+
+    /// ## Tips & Behaviour
+    /// 
+    /// * Short named options `-o` are case sensitive: `-a != -A`
+    /// * Long named options `--option` are case insensitive: `--opt == --Opt`
+    /// * Options can be combined by the user: `-a -b -c` -> `-abc`
+    /// * Combined options can end with an option-parameter: `-abco value`
+    /// * Option-parameters have several forms: `-o value` `-o=value` `--option value` `--option=value`
+    /// * Quotes and Escale characters are both supported: `--option "my value"` `--option \-my-value`
 }
