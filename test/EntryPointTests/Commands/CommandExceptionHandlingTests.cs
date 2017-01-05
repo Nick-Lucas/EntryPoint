b0600166ff889b92d4776e31c6b76a88762ba2f3
Nick Lucas <nicholas.lucas06@gmail.com>
2017-01-05 21:41:56 +0000

    Rethrow exceptions properly to preserve the stacktrace. Closes #29

---
+++ test/EntryPointTests/Commands/CommandExceptionHandlingTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using EntryPoint.Commands;
+using Xunit;
+using EntryPoint;
+using EntryPointTests.Commands.BaseCommandsHelpers;
+
+namespace EntryPointTests.Commands {
+    public class CommandExceptionHandlingTests {
+        [Fact]
+        public void CommandException_Simple() {
+            Assert.Throws<DivideByZeroException>(
+                () => Cli.Execute<CommandModel_ExceptionThrow>(new string[] { }));
+        }
+
+        [Fact]
+        public void CommandException_InspectStacktrace() {
+            var e = Assert.Throws<DivideByZeroException>(
+                () => Cli.Execute<CommandModel_ExceptionThrow>(new string[] { }));
+
+            for (int i = 1; i <= 4; i++) {
+                Assert.Contains($"Layer{i}", e.StackTrace, StringComparison.CurrentCultureIgnoreCase);
+            }
+        }
+    }
+}
