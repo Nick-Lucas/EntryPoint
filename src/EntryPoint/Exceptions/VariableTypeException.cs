f44dc1084716bf7d00f99a59f0b66b10622cf1b4
Nick Lucas <nicholas.lucas06@gmail.com>
2017-03-25 15:15:15 +0000

    Add VariableTypeException < UserFacingException for failed type conversions

---
+++ src/EntryPoint/Exceptions/VariableTypeException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntryPoint.Exceptions {
+    public class VariableTypeException : UserFacingException {
+        internal VariableTypeException(string message) : base(message) { }
+    }
+}
