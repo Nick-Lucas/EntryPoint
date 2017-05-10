d97eb183d2840ca83c6855d67657be7d5c40f7ea
Łukasz Pyrzyk <pyrzykl@ryanair.com>
2017-05-10 11:43:11 +0200

    Revert "Class cleanup"
    This reverts commit c1f5c64fff7cd8958f16f011d64c89f632cb851b.

--- src/EntryPoint/Arguments/OptionStrategies/OptionStrategy.cs
+++ src/EntryPoint/Arguments/OptionStrategies/OptionStrategy.cs
@@ -1,41 +1,51 @@
 ﻿using System;
-using EntryPoint.Parsing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace EntryPoint.Arguments.OptionStrategies
-{
-    internal class OptionStrategy : IOptionStrategy
-    {
-        public object GetValue(Option modelOption, TokenGroup tokenGroup)
-        {
-            var value = HasDoubleOption(tokenGroup.Option)
+using EntryPoint.Common;
+using EntryPoint.Parsing;
+using EntryPoint.Arguments;
+
+namespace EntryPoint.Arguments.OptionStrategies {
+
+    internal class OptionStrategy : IOptionStrategy {
+        internal OptionStrategy() { }
+
+        public object GetValue(Option modelOption, TokenGroup tokenGroup) {
+            var value = HasDoubleOption(tokenGroup.Option, modelOption.Definition) 
                      || HasSingleOption(tokenGroup.Option, modelOption.Definition.ShortName);
             return CheckValue(value, modelOption.Property.PropertyType, modelOption.Definition);
         }
 
-        public bool RequiresParameter => false;
+        public bool RequiresParameter {
+            get {
+                return false;
+            }
+        }
+
 
         // ** Helpers **
 
-        bool HasSingleOption(Token arg, char? argName)
-        {
-            return argName != null && arg.IsSingleDashOption();
+        bool HasSingleOption(Token arg, char? argName) {
+            if (argName == null) {
+                return false;
+            }
+            return arg.IsSingleDashOption();
         }
 
-        bool HasDoubleOption(Token arg)
-        {
+        bool HasDoubleOption(Token arg, BaseOptionAttribute definition) {
             return arg.IsDoubleDashOption();
         }
 
-        object CheckValue(bool value, Type outputType, BaseOptionAttribute definition)
-        {
-            if (outputType != typeof(bool))
-            {
+        object CheckValue(bool value, Type outputType, BaseOptionAttribute definition) {
+            if (outputType != typeof(bool)) {
                 throw new InvalidOperationException(
                     $"The type of {Cli.DASH_DOUBLE}{definition.LongName} on the ArgumentsModel, "
                     + $"must be a boolean for {nameof(OptionAttribute)}. Use {nameof(OptionParameterAttribute)} instead");
             }
-
             return value;
         }
     }
+
 }
